762249adeac7b47fe67510ecac440951465fb7d6
liangjiange <liangjiange@gmail.com>
2020-02-04 19:13:05 +0800

    前端示例

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from "vue";
+
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/zh-CN"; // lang i18n
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 
+Vue.use(ElementUI, { locale });
 Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
+axios.defaults.baseURL = "http://localhost:9000";
+axios.defaults.timeout = 1000;
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-col>
+      <el-col :span="12"
+        ><el-input v-model="inputUsername" placeholder="请输入用户名"></el-input
+      ></el-col>
+      <el-col :span="12"
+        ><el-button type="primary" @click="doSearch"
+          >查找用户详细信息</el-button
+        ></el-col
+      >
+    </el-col>
+    <div>
+      <div>{{ user.id }}</div>
+      <div>{{ user.username }}</div>
+      <div>{{ user.email }}</div>
+      <div>{{ user.cellphone }}</div>
+      <div>{{ user.createTime }}</div>
+      <div>{{ user.updateTime }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      user: { username: "zhangshan", cellphone: "13456789000" },
+      inputUsername: ""
+    };
+  },
+  methods: {
+    doSearch: function() {
+      var inputUsername = this.inputUsername;
+      this.$http({
+        method: "get",
+        url: "/api/users?username=" + inputUsername
+      }).then(
+        response => {
+          console.log("请求成功");
+          console.log(response);
+          this.user = response.data;
+        },
+        response => {
+          console.log("请求失败");
+          console.log(response);
+        }
+      );
+    }
   }
 };
 </script>
